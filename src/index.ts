c4e29586409cac609aa3e94c65f5ee1774ac44c8
digoarthur <digo.arthur@hotmail.com>
2025-08-14 21:30:21 +0100

    fix: remove deprecated stackIconsURL

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { StackIcons } from './components/StackIcons';
 export { StackLabels } from './components/StackLabels/StackLabels';
-export { stackIconsURL } from './icons/stackIconsURL';
 export { useGitHubAutomatedRepos, IGitHubRepos } from './hook/useGithubAutomatedRepos';
