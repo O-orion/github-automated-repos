a4b4bee1d8ac95eec492b65ee6e1875e4f8a1fc0
digoarthur <digo.arthur@hotmail.com>
2025-08-14 21:13:42 +0100

    feat: add StackIcons and StackLabels components

--- src/react-query.ts
+++ src/react-query.ts
@@ -1,3 +1,5 @@
 // src/react-query.ts
 export { useGitHubAutomatedRepos } from './hook/react-query/useGithubAutomatedRepos';
 export { IGitHubRepos } from './hook/react-query/useGithubAutomatedRepos'; 
+export { StackIcons } from './components/StackIcons';
+export { StackLabels } from './components/StackLabels/StackLabels';
